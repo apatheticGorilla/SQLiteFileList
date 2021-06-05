f7ac7dd615925d3410ff59c135215d84c00e4580
apatheticGorilla <83184263+apatheticGorilla@users.noreply.github.com>
2021-06-05 11:37:40 -0600

    added folders table to database

--- databaseManager.py
+++ databaseManager.py
@@ -8,54 +8,84 @@ cur = con.cursor()
 def addToDatabase(path):
 	items = os.listdir(path)
 	# print(items)
-	try:
-		for item in items:
-			filePath = os.path.join(path, item)
-			if os.path.isfile(filePath):
-				insertRecord(filePath)
+	for item in items:
+		try:
+			filepath = os.path.join(path, item)
+			if os.path.isfile(filepath):
+				insertFileRecord(filepath)
 			else:
 				try:
-					addToDatabase(filePath)
+					insertFolderRecord(filepath)
+					addToDatabase(filepath)
 				except PermissionError:
+					print("permission error")
 					continue
-	
-	except FileNotFoundError:
-		print("file not found: ", item)
+		except FileNotFoundError:
+			print("file not found: ", item)
 
 
 def updateDataBase(paths):
 	print("Deleting data")
 	cur.execute('DELETE FROM files;')
+	cur.execute('DELETE FROM folders')
 	con.commit()
 	for path in paths:
 		print("enumerating ", path)
 		addToDatabase(path)
+	print("committing")
 	con.commit()
 	print("vacuuming")
 	vacuum()
 	print("done")
 
 
-def insertRecord(filePath):
-	isNull = False
+def getParentIndex(path):
+	# print(path)
+	parentPath = os.path.dirname(path)
+	
+	# response = executeQuery("SELECT folder_id FROM folders WHERE folder_path = \"" + parentPath + "\";")
+	try:
+		result = cur.execute("SELECT folder_id FROM folders WHERE folder_path = '" + parentPath + "' LIMIT 1;")
+		for r in result:
+			(index, *rest) = r
+			if r is None:
+				return None
+			return str(index)
+	except sqlite3.OperationalError:
+		return None
+
+
+def insertFileRecord(filePath):
+	isnull = False
+	parent = os.path.dirname(filePath)
 	try:
 		name = os.path.basename(filePath)
 		extension = name[name.rindex('.'):]
 	except ValueError:
-		isNull = True
+		isnull = True
 	try:
 		size = os.path.getsize(filePath)
-		if isNull:
-			statement = "INSERT INTO files VALUES(NULL,\"" + filePath + "\", NULL,'" + str(size) + "')"
-			cur.execute(statement)
+		if isnull:
+			cur.execute(
+				"INSERT INTO files VALUES(NULL,\"" + filePath + "\", NULL,'" + str(size) + "',\"" + parent + "\")")
 		else:
 			cur.execute(
 				"INSERT INTO files VALUES(NULL,\"" + filePath + "\",'" + extension + "','" + str(
-					size) + "')")
-	except sqlite3.OperationalError:
-		print('failed to add file: ', filePath)
+					size) + "',\"" + parent + "\")")
 	except FileNotFoundError:
 		print('file decided it didn\'t exist: ', filePath)
+
+
+def insertFolderRecord(path):
+	statement = "INSERT INTO folders VALUES(NULL, \"" + path + "\",\"" + os.path.join(path) + "\");"
+	cur.execute(statement)
+
+
+# parent = getParentIndex(path)
+# if parent is None:
+# 	cur.execute("INSERT INTO folders VALUES(NULL, \"" + path + "\",NULL);")
+# else:
+# 	cur.execute("INSERT INTO folders VALUES(NULL, \"" + path + "\", '" + parent + "');")
 
 
 def vacuum():
@@ -64,17 +94,30 @@ def vacuum():
 
 
 def createDatabase():
-	cur.execute("DROP TABLE files;")
+	print("recreating tables")
+	cur.execute("DROP TABLE IF EXISTS files;")
+	cur.execute("DROP TABLE IF EXISTS folders;")
 	con.commit()
 	cur.execute('''
 		CREATE TABLE IF NOT EXISTS files(
 			file_id INTEGER PRIMARY KEY AUTOINCREMENT,
 			file_path TEXT,
 			extension TEXT,
-			size INT
+			size INT,
+			parent TEXT,
+			FOREIGN KEY (parent) REFERENCES folders (folder_path)
 		);
 	''')
+	cur.execute('''
+	CREATE TABLE folders(
+		folder_id INTEGER PRIMARY KEY AUTOINCREMENT,
+		folder_path TEXT,
+		parent TEXT,
+		FOREIGN KEY (parent) REFERENCES folders(folder_path)
+	);
+	''')
 	con.commit()
+	vacuum()
 
 
 def executeQuery(query):
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import sys
 import databaseManager
-
+import gc
 
 db = databaseManager
-# db.createDatabase()
+db.createDatabase()
 # , "D:\\", "E:\\", "G:\\", "F:\\"
-drives = ["C:\\"]
-# db.updateDataBase(drives)
-tuples = db.executeQuery("SELECT file_path FROM files WHERE extension IS NULL")
-for item in tuples:
-	(filePath, *rest) = item
-	print(filePath)
+drives = ["C:\\", "D:\\", "E:\\", "G:\\", "F:\\"]
+db.updateDataBase(drives)
+# tuples = db.executeQuery("SELECT file_path FROM files WHERE extension IS NULL")
+# for item in tuples:
+# 	(filePath, *rest) = item
+# 	print(filePath)
+# print(db.executeQuery("SELECT * FROM folders"))
