37987007ab4add4cfba0930e8a9b4fecf2105128
apatheticGorilla <83184263+apatheticGorilla@users.noreply.github.com>
2021-06-29 10:32:12 -0600

    improved exportToCSV

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ def time_convert(sec):
 	print("Time Lapsed = {0}:{1}:{2}".format(int(hours), int(mins), sec))
 
 
-def exportTOCSV(header: str, path: str, data: list[tuple]):
-	file = open(path)
+def exportToCSV(header: str, path: str, data: list[tuple]):
+	file = open(path, 'w', encoding="UTF-16")
 	file.write(header + '\n')
 	for d in data:
 		line = ""
 		for t in d:
-			line = line + t + ','
+			line = line + str(t) + ','
 		line = line[0:len(line) - 1]
-		file.write(line)
+		file.write(line + "\n")
 	file.close()
+	print("exported to:", path)
 
 
 db = databaseManager
