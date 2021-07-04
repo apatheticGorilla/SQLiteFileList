e9f06671ce63828beb56f954282a4850889de7e8
apatheticGorilla <83184263+apatheticGorilla@users.noreply.github.com>
2021-07-04 11:19:27 -0600

    moved exportToCSV to util.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import databaseManager
+import util
 
 
 # import Timer
@@ -12,40 +13,20 @@ def time_convert(sec):
 	print("Time Lapsed = {0}:{1}:{2}".format(int(hours), int(mins), sec))
 
 
-def exportToCSV(header: str, path: str, data: list[tuple]):
-	file = open(path, 'w', encoding="UTF-16")
-	file.write(header + '\n')
-	for d in data:
-		line = ""
-		for t in d:
-			line = line + str(t) + ','
-		line = line[0:len(line) - 1]
-		file.write(line + "\n")
-	file.close()
-	print("exported to:", path)
-
-
 db = databaseManager
 
-# timer = Timer.Timer()
-# timer.startTime()
 # db.createDatabase()
-# # , "D:\\", "E:\\", "G:\\", "F:\\"
-drives = ["C:\\", "D:\\", "G:\\", "F:\\", "z:\\"]
-
-# db.addFolder("Z:\\")
-# db.vacuum()
+# drives = ["C:\\", "D:\\", "G:\\", "F:\\", "Z:\\"]
 # db.updateDataBase(drives)
-simpleFile = open("C:\\temp\\files.tfl", 'w', encoding="UTF-8")
-fileList = db.executeQuery("SELECT file_path FROM files WHERE size <= 4096")
-for f in fileList:
-	(line, *drop) = f
-	simpleFile.write(line + "\n")
-simpleFile.close()
-# db.addFolder("C:\\")
-# time_convert(timer.stopTime())
-# files = db.executeQuery("SELECT * FROM files")
-# files = db.filesWithExtension(".ttf")
-# exportTOCSV("file_id,path,extension,size,parent", "C:\\Temp\\all.csv", files)
+util.pieChartByExtension(1000)
 
-
+# print(db.executeQuery("SELECT COUNT(file_id) FROM files WHERE extension IS NULL"))
+# simpleFile = open("C:\\temp\\files2.tfl", 'w', encoding="UTF-8")
+# fileList = db.executeQuery("SELECT file_path FROM files WHERE size BETWEEN 5242880 AND 10485760 ORDER BY file_path ASC")
+# for f in fileList:
+# 	(Line, *drop) = f
+# 	simpleFile.write(Line + "\n")
+# simpleFile.close()
+# db.execute("""DELETE FROM files WHERE file_path LIKE 'Z:\\%';
+# 			DELETE FROM folders WHERE folder_path LIKE 'Z:\\%';""", True)
+# db.vacuum()
