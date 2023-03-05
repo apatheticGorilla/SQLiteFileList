65eadaa4c405afb0b1fb6f6ce0ee0d0126986c9f
apatheticGorilla <colin.behunin@gmail.com>
2023-03-05 11:39:20 -0700

    removed hard-coded home directory

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os.path as path
+
 import databaseManager
 import util
 
-
 import Timer
+
 
 def time_convert(sec):
 	mins = sec // 60
@@ -11,15 +13,18 @@ def time_convert(sec):
 	mins = mins % 60
 	print("Time Lapsed = {0}:{1}:{2}".format(int(hours), int(mins), sec))
 
-db = databaseManager.databaseManager('C:\\Temp\\files.db', "C:\\Temp\\databaseManger.log")
-# db = databaseManager.databaseManager('/home/hello/Documents/files.db', "/home/hello/Documents/databaseManger.log")
+
+# db = databaseManager.databaseManager('C:\\Temp\\files.db', "C:\\Temp\\databaseManager.log")
+home = path.expanduser("~")
+dbFolder = path.join(home, 'Documents/SQLiteDatabase')
+db = databaseManager.databaseManager(path.join(dbFolder, 'files.db'), path.join(dbFolder, 'logs/databaseManager.log'))
 drives = ["C:\\", "D:\\", "F:\\", 'G:\\', 'E:\\']
 # db.createDatabase()
 timer = Timer.Timer()
 timer.startTime()
-db.updateDataBase(drives, 0)
+# db.updateDataBase(drives, 0)
+db.updateDataBase(['/'], 0)
 print(time_convert(timer.stopTime()))
-# db.updateDataBase([\], 0)
 
 # util.writeTFLList("C:\\temp\\cabfiles.tfl", "extension = '.jar' ORDER BY size DESC")
 # db.execute("""DELETE FROM files WHERE file_path LIKE 'Z:\\%';
